179315608d0c06aefe104e66c73d907cca4eca92
KararTY <alremahykarar@ymail.com>
2017-03-02 06:18:19 +0100

    boop

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,47 +1,57 @@
 let game = {}
 
 game.create = function () {
+  // Shorthand for this.game
   let kek = this.game
+  // Shorter variables for centering visual objects.
   let centerScreenX = kek.world.centerX
   let centerScreenY = kek.world.centerY
+
+  // Pepe in the corner.
   let logo = kek.add.sprite(0, centerScreenY - 100, 'logo')
   logo.anchor.setTo(0, 0)
 
+  // Create a basic css for text.
   let gameTitleStyle = { font: 'Bangers-Regular', fill: '#fff' }
+  // Make the game title.
   let gameTitle = kek.add.text(centerScreenX, 50, '#RESISTJAM', gameTitleStyle)
   gameTitle.fontSize = '4rem'
-  gameTitle.anchor.setTo(0.5, 0.5)
-  gameTitle.padding.set(10, 16)
+  gameTitle.anchor.setTo(0.5, 0.5) // Anchor in the middle of the text.
+  gameTitle.padding.set(10, 16) // Padding to prevent text being cut off.
 
-  // Create menu.
+  // Create menu array with objects.
   let menu = [
     {title: 'Play', function: 'play'},
     {title: 'Credits', function: 'credits'}
   ]
   let itemMargin = {starting: 125, margin: 50}
+  // Create a group for menu items that can later be manipulated
   let menuGroup = kek.add.group()
 
+  // This triggers on input down when selecting a menu item.
   let selectMenuItem = {
     down: (child) => {
       let item = child.details
       kek.state.start(item.function)
     }
   }
-
+  // Create menu text by looping through the menu array objects.
   for (let i = 0; i < menu.length; i++) {
     let element = menu[i]
     var theMenuItem = kek.add.text(centerScreenX, itemMargin.starting, '', {font: '3rem Bangers-Regular', fill: '#fff'})
     theMenuItem.text = element.title
     theMenuItem.anchor.setTo(0.5, 0.5)
     theMenuItem.padding.set(10, 16)
-    theMenuItem.details = {function: element.function}
-    theMenuItem.inputEnabled = true
+    theMenuItem.details = {function: element.function} // Attach an object containing objects to text.
+    theMenuItem.inputEnabled = true // Enable input for the text.
     itemMargin.starting += itemMargin.margin
 
     theMenuItem.events.onInputDown.add(selectMenuItem.down, this)
+    // Add a child (whatever menu array object were looping through right now) to group.
     menuGroup.addChild(theMenuItem)
   }
 
+  // Create copyright text.
   let copyText = kek.add.text(centerScreenX, window.innerHeight, '', {font: '2rem Bangers-Regular', fill: '#fff'})
   copyText.anchor.setTo(0.5, 1)
   copyText.text = 'Copyrighted 2017 Team Kekistan'
--- src/js/game/states/play.js
+++ src/js/game/states/play.js
@@ -1,5 +1,7 @@
 let play = {}
+let Phaser = Phaser || {}
 
+// Global scope the variables.
 let player
 let testBox
 let cursors
@@ -9,11 +11,16 @@ let jumpTimer = 0
 
 play.create = function () {
   let kek = this.game
+  let centerScreenX = kek.world.centerX
+  /* Not used
+   let centerScreenY = kek.world.centerY */
+
+  // Start up the physics system
+  // (Should switch to P2 before we do anything complex)
   kek.physics.startSystem(Phaser.Physics.ARCADE)
   kek.physics.arcade.gravity.y = 300
-  let centerScreenX = kek.world.centerX
-  let centerScreenY = kek.world.centerY
 
+  // Make an immovable box.
   testBox = kek.add.sprite(centerScreenX, window.innerHeight - 250, 'box')
   testBox.anchor.setTo(0.5, 0)
   kek.physics.enable(testBox, Phaser.Physics.ARCADE)
@@ -21,23 +28,29 @@ play.create = function () {
   testBox.body.static = true
   testBox.body.immovable = true
 
+  // Make the player.
   player = kek.add.sprite(250, 250, 'box')
   kek.physics.enable(player, Phaser.Physics.ARCADE)
   player.body.collideWorldBounds = true
   player.body.gravity.y = 1000
   player.body.maxVelocity.y = 900
 
+  // Enable cursor keys.
   cursors = kek.input.keyboard.createCursorKeys()
   jumpButton = kek.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 }
 
+// Think of this function as an endless loop.
 play.update = function () {
   let kek = this.game
 
+  // Check for collision between player and testbox.
   kek.physics.arcade.collide(testBox, player)
 
+  // Unless something is changing player's X velocity, have it be '0'
   player.body.velocity.x = 0
 
+  // On cursor keys left <-
   if (cursors.left.isDown) {
     player.body.velocity.x = -150
 
@@ -45,6 +58,8 @@ play.update = function () {
       player.animations.play('left')
       facing = 'left'
     }
+  // On cursor keys right ->
+  // There's sample code for animations here.
   } else if (cursors.right.isDown) {
     player.body.velocity.x = 150
 
@@ -52,6 +67,8 @@ play.update = function () {
       player.animations.play('right')
       facing = 'right'
     }
+  // Otherwise idle character
+  // There's sample code for animations here.
   } else {
     if (facing !== 'idle') {
       player.animations.stop()
@@ -64,12 +81,14 @@ play.update = function () {
       facing = 'idle'
     }
   }
-  if (jumpButton.isDown && player.body.touching.down || player.body.onFloor() && kek.time.now > jumpTimer) {
+  // Bouncyness fixed by gnomesort
+  if (jumpButton.isDown && player.body.velocity.y === 0 && kek.time.now > jumpTimer) {
     player.body.velocity.y = -900
     jumpTimer = kek.time.now + 750
   }
 }
 
+// Render is mostly used for debugging.
 play.render = function () {
   let kek = this.game
   kek.debug.bodyInfo(testBox, 16, 24)
