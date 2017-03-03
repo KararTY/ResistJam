0f2457ad65c129035b39d299aad6553f83d38255
taimensenescence <gnomesort@redchan.it>
2017-03-03 14:49:53 -0800

    Fix event based movement Uses the new Character and Controller objects to control movement Movement is all event based Also reset the player sprite to a box because the circle is buggy beyond belief

--- src/js/game/states/play.js
+++ src/js/game/states/play.js
@@ -1,43 +1,11 @@
-/**
- * Movement constants can be combined using bitwise OR
- * for example UP (0001) | LEFT (0100) will result in the value UPLEFT (0101)
- * Movement constants can be checked using bitwise and
- * for example if(direction & (UP | LEFT)) is true if direction has the up and left values set
- * if(directions & LEFT) is true if the left value is set regardless of other values
- */
-const DIRECTIONS = Object.freeze({
-  /**
-  * Constant representing no movement
-  */
-  'NONE': 0,
-
-  /**
-  * Constant representing upward movement
-  */
-  'UP': 1,
-
-  /**
-  * Constant representing leftward movement
-  */
-  'LEFT': 4,
-
-  /**
-  * Constant representing downward movement
-  */
-  'DOWN': 2,
-
-  /**
-  * Constant representing rightward movement
-  */
-  'RIGHT': 8
-})
+var Character = require('../objs/character')
 
 let play = {}
 
 play.create = function () {
   let kek = this.game
   this.testBox
-  this.player = new Character(kek)
+
   let centerScreenX = kek.world.centerX
   /* Not used
    let centerScreenY = kek.world.centerY */
@@ -56,10 +24,33 @@ play.create = function () {
   this.testBox.body.immovable = true
 
   // Make the player.
-  this.player.sprite = kek.add.sprite(255, 255, 'character')
+  this.player = new Character(kek, undefined, kek.add.sprite(256, 256, 'character'))
+  // Controller to action binding
+  // As the game progresses we may want to move this binding into character.js
+  // Until we know what the characters actions are this will do
+  this.player.controller.up.onDown.add(function (data) { if (this.sprite.body.velocity.y === 0) { this.sprite.body.velocity.y = -900 } }, this.player)
+  this.player.controller.left.onDown.add(function (data) {
+    this.sprite.body.velocity.x = -150
+    if (this.sprite.previousPosition.x > this.sprite.position.x) {
+      this.animations.play('left')
+    }
+  }, this.player)
+  this.player.controller.left.onUp.add(function (data) {
+    if (!this.controller.right.isDown) { this.sprite.body.velocity.x = 0 }
+  }, this.player)
+  this.player.controller.right.onDown.add(function (data) {
+    this.sprite.body.velocity.x = 150
+    if (this.sprite.previousPosition.x < this.sprite.position.x) {
+      this.sprite.animations.play('right')
+    }
+  }, this.player)
+  this.player.controller.right.onUp.add(function (data) {
+    if (!this.controller.left.isDown) { this.sprite.body.velocity.x = 0 }
+  }, this.player)
+  this.player.controller.jump.onDown = this.player.controller.up.onDown
   this.player.sprite.anchor.setTo(0.5)
   kek.physics.enable(this.player.sprite, Phaser.Physics.ARCADE)
-  this.player.sprite.body.setCircle(120)
+  this.player.sprite.body.setSize(256, 256)
   this.player.sprite.body.collideWorldBounds = true
   this.player.sprite.body.gravity.y = 1000
   this.player.sprite.body.maxVelocity.y = 900
@@ -75,11 +66,8 @@ play.update = function () {
 
   // Otherwise idle character
   // There's sample code for animations here.
-  if (this.player.facing !== DIRECTIONS.NONE) {
+  if (this.player.previousPosition === this.player.position) {
     this.player.sprite.animations.stop()
-    if (this.player.facing === DIRECTIONS.LEFT) this.player.frame = 0
-    else this.player.sprite.frame = 5
-    this.player.facing = DIRECTIONS.NONE
   }
 }
 
@@ -88,76 +76,6 @@ play.render = function () {
   let kek = this.game
   kek.debug.bodyInfo(this.testBox, 16, 24)
   kek.debug.body(this.player.sprite)
-}
-
-// TODO
-// Move this to its own file
-var Character = function (game) {
-  this.game = game
-  this.facing = DIRECTIONS.LEFT
-  this.controls = this.bindKeys()
-  console.log(this.controls)
-  this.sprite = null
-}
-Character.prototype.sprite
-Character.prototype.game
-Character.prototype.facing
-Character.prototype.controls
-/**
- * Bind keys and set up keyboard movement events.
- * Binds both WASD and Arrow Key controls
- * Binds jump to SPACEBAR
- */
-Character.prototype.bindKeys = function () {
-  // Create key bindings
-  let controls = this.game.input.keyboard.addKeys({
-    'up': Phaser.KeyCode.W,
-    'left': Phaser.KeyCode.A,
-    'down': Phaser.KeyCode.S,
-    'right': Phaser.KeyCode.D,
-    'altup': Phaser.KeyCode.UP,
-    'altleft': Phaser.KeyCode.LEFT,
-    'altdown': Phaser.KeyCode.DOWN,
-    'altright': Phaser.KeyCode.RIGHT,
-    'jump': Phaser.KeyCode.SPACEBAR
-  })
-
-  // Add movement events
-  // Jumping
-  controls.up.onDown.add(function (data) { if (this.sprite.body.velocity.y === 0) { this.sprite.body.velocity.y = -900 } }, this)
-  // Moving Left
-  controls.left.onDown.add(function (data) {
-    this.sprite.body.velocity.x = -150
-    if (this.facing !== DIRECTIONS.LEFT) {
-      this.animations.play('left')
-      this.facing = DIRECTIONS.LEFT
-    }
-  }, this)
-  // Clearing movement
-  controls.left.onUp.add(function (data) {
-    this.sprite.body.velocity.x = 0
-  }, this)
-  // Moving Right
-  controls.right.onDown.add(function (data) {
-    this.sprite.body.velocity.x = 150
-    if (this.facing !== DIRECTIONS.RIGHT) {
-      this.sprite.animations.play('right')
-      this.facing = DIRECTIONS.RIGHT
-    }
-  }, this)
-  controls.right.onUp = controls.left.onUp
-
-  // Bind Alternates
-  controls.altup.onDown = controls.up.onDown
-  controls.altleft.onDown = controls.left.onDown
-  controls.altleft.onUp = controls.left.onUp
-  controls.altright.onDown = controls.right.onDown
-  controls.altright.onUp = controls.left.onUp
-
-  // Bind action keys
-  controls.jump.onDown = controls.up.onDown
-
-  return controls
 }
 
 module.exports = play
